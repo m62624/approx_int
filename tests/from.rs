7ee3c8eea8514f760c056c49f2876250d8c798a8
Mansur Azatbek <mansur62624@gmail.com>
2024-08-30 21:19:13 +0500

    test: add new integration tests

---
+++ tests/from.rs
@@ -0,0 +1,29 @@
+use approx_int::SmallValue;
+
+#[test]
+fn tuple_t_0() {
+    let small_value = SmallValue::new(128);
+    let tuple: (u8, u8, bool) = small_value.into();
+    assert_eq!(tuple, (8, 50, false));
+}
+
+#[test]
+fn tuple_t_1() {
+    let small_value = SmallValue::new(64);
+    let tuple: (u8, u8, bool) = small_value.into();
+    assert_eq!(tuple, (7, 50, false));
+}
+
+#[test]
+fn tuple_t_2() {
+    let tuple = (8, 50, false);
+    let small_value: SmallValue<u32> = tuple.into();
+    assert_eq!(small_value.min_bits(), 8);
+}
+
+#[test]
+fn tuple_t_3() {
+    let value = u128::MAX;
+    let small_value: SmallValue<u128> = value.into();
+    assert_eq!(small_value.min_bits(), 128);
+}
