37fd5bc84c564d47d2fc3da52810b05c94202dd7
Mansur Azatbek <mansur62624@gmail.com>
2024-09-01 15:09:31 +0500

    test: add new integration tests

---
+++ tests/special_values.rs
@@ -0,0 +1,33 @@
+use approx_int::{SmallValue, SpecialBytes};
+
+fn calculate_error_rate<T: SpecialBytes>(original: T, approximate: T) -> Option<f64> {
+    if original.is_zero() {
+        None
+    } else {
+        Some(((original - approximate).to_f64()? / original.to_f64()?) * 100.0)
+    }
+}
+
+#[test]
+fn special_value_t_0() {
+    let original = 314265047194861710702393853117681977664u128;
+    let approx = SmallValue::new(original).approximate();
+    let error_rate = calculate_error_rate(original, approx).unwrap();
+    assert!(error_rate < 0.9);
+}
+
+#[test]
+fn special_value_t_1() {
+    let original = 283133942661148271300918452068693765763u128;
+    let approx = SmallValue::new(original).approximate();
+    let error_rate = calculate_error_rate(original, approx).unwrap();
+    assert!(error_rate < 0.9);
+}
+
+#[test]
+fn special_value_t_2() {
+    let original = 188221493376348417689459869480508947014u128;
+    let approx = SmallValue::new(original).approximate();
+    let error_rate = calculate_error_rate(original, approx).unwrap();
+    assert!(error_rate < 0.9);
+}
