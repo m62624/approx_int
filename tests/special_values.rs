0bbd9e177ddb7e45fc2636607ac7559f5efbdf4b
Mansur Azatbek <mansur62624@gmail.com>
2024-09-01 17:30:21 +0500

    test: add new integration tests

--- tests/special_values.rs
+++ tests/special_values.rs
@@ -47,3 +47,67 @@ fn special_value_t_4() {
     let error_rate = calculate_error_rate(original, approx).unwrap();
     assert!(error_rate < 1.0);
 }
+
+#[test]
+fn special_value_t_5() {
+    for i in -1000..-5 {
+        let original = i;
+        let approx = SmallValue::new(original).approximate();
+        let error_rate = calculate_error_rate(original, approx).unwrap();
+        assert!(error_rate < 10.0);
+    }
+}
+
+#[test]
+fn special_value_t_6() {
+    let original = i32::MIN;
+    assert!(calculate_error_rate(original, SmallValue::new(original).approximate()).unwrap() < 2.0);
+}
+
+#[test]
+fn special_value_t_7() {
+    let original = i32::MAX;
+    assert!(calculate_error_rate(original, SmallValue::new(original).approximate()).unwrap() < 2.0);
+}
+
+#[test]
+fn special_value_t_8() {
+    let original = i64::MIN;
+    assert!(calculate_error_rate(original, SmallValue::new(original).approximate()).unwrap() < 2.0);
+}
+
+#[test]
+fn special_value_t_9() {
+    let original = i64::MAX;
+    assert!(calculate_error_rate(original, SmallValue::new(original).approximate()).unwrap() < 2.0);
+}
+
+#[test]
+fn special_value_t_10() {
+    let original = i128::MIN;
+    assert!(calculate_error_rate(original, SmallValue::new(original).approximate()).unwrap() < 2.0);
+}
+
+#[test]
+fn special_value_t_11() {
+    let original = i128::MAX;
+    assert!(calculate_error_rate(original, SmallValue::new(original).approximate()).unwrap() < 2.0);
+}
+
+#[test]
+fn special_value_t_12() {
+    let original = u32::MAX;
+    assert!(calculate_error_rate(original, SmallValue::new(original).approximate()).unwrap() < 2.0);
+}
+
+#[test]
+fn special_value_t_13() {
+    let original = u64::MAX;
+    assert!(calculate_error_rate(original, SmallValue::new(original).approximate()).unwrap() < 2.0);
+}
+
+#[test]
+fn special_value_t_14() {
+    let original = u128::MAX;
+    assert!(calculate_error_rate(original, SmallValue::new(original).approximate()).unwrap() < 2.0);
+}
